709e164ab0540547c00353f271727e131f46229e
Shlomi Kushchi <shlomik@gmail.com>
2020-03-07 21:34:42 +0200

    using the stream posts functionality

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-from simple_pastebin_parser.simple_pastebin_parser import parse
+from simple_pastebin_parser.simple_pastebin_parser import get_posts
 
 
 if __name__ == '__main__':
-    parse()
+    for post in get_posts(True, 30):
+        # print("href: ", href)
+        print("Title: ", post.title)
+        print("Author: ", post.author)
+        print("date: ", post.date)
+        print(post.code)
+        print("*" * 20)
+        print("*" * 20)
