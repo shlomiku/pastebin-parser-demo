ed03e39a787b71afdfc21d7b877d243661985065
Shlomi Kushchi <shlomik@gmail.com>
2020-03-08 21:43:38 +0200

    use the href id as paste id

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import hashlib
 import json
 from os.path import exists, join
 from os import getcwd,mkdir
@@ -13,14 +12,8 @@ def create_output_folder_if_doesnt_exist():
         mkdir(OUTPUT_FOLDER)
 
 
-def get_paste_hash(paste):
-    data = paste.get()
-    data["Date"] = str(data["Date"])
-    return hashlib.md5("+".join(data.values()).encode('utf-8')).hexdigest()
-
-
 def does_paste_file_already_exist(paste):
-    filename = "{}.json".format(get_paste_hash(paste))
+    filename = "{}.json".format(paste.id)
     file_path = join(OUTPUT_FOLDER, filename)
     return exists(file_path)
 
@@ -28,7 +21,7 @@ def does_paste_file_already_exist(paste):
 def store_paste_date_to_disk(paste):
     data = paste.get()
     data["Date"] = str(data["Date"])
-    filename = "{}.json".format(get_paste_hash(paste))
+    filename = "{}.json".format(paste.id)
     file_path = join(OUTPUT_FOLDER, filename)
     with open(file_path, 'w') as f:
         json.dump(data, f, indent=4)
@@ -36,6 +29,6 @@ def store_paste_date_to_disk(paste):
 
 if __name__ == '__main__':
     create_output_folder_if_doesnt_exist()
-    for paste in simple_pastebin_parser.get_posts(True, 60*2):
+    for paste in simple_pastebin_parser.get_pastes(should_stream=True, sampling_frequency=60*2):
         if not does_paste_file_already_exist(paste):
             store_paste_date_to_disk(paste)
