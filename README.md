faabf547f315a0b72be2acd056f925f84a63d85a
Shlomi Kushchi <shlomik@gmail.com>
2020-03-08 20:52:15 +0200

    v0.1.0 - basic required implementation

---
+++ README.md
@@ -0,0 +1,20 @@
+[![Build Status](https://travis-ci.org/shlomikushchi/Simple-Pastebin-Parser.svg?branch=master)](https://travis-ci.org/shlomikushchi/Simple-Pastebin-Parser)
+
+# Demo project for the simple-pastebin-parser project
+
+you can check out the project [here](https://github.com/shlomikushchi/simple-pastebin-parser)
+
+installation:
+---
+`pip install -r requirements.txt`
+will install the latest version of simple-pastebin-parser
+
+  Release notes:
+===
+
+v0.1.0 - basic requirements
+---
+basic required implementation. using the simple-pastebin-parser to sample pastebin.com every 2 minutes
+and storing the files to an OUTPUT local folder. 
+
+run it like so: `python main.py`
--- main.py
+++ main.py
@@ -1,12 +1,41 @@
+import hashlib
+import json
+from os.path import exists, join
+from os import getcwd,mkdir
+
 import simple_pastebin_parser
+
+OUTPUT_FOLDER = join(getcwd(), "OUTPUT")
+
+
+def create_output_folder_if_doesnt_exist():
+    if not exists(OUTPUT_FOLDER):
+        mkdir(OUTPUT_FOLDER)
+
+
+def get_paste_hash(paste):
+    data = paste.get()
+    data["Date"] = str(data["Date"])
+    return hashlib.md5("+".join(data.values()).encode('utf-8')).hexdigest()
+
+
+def does_paste_file_already_exist(paste):
+    filename = "{}.json".format(get_paste_hash(paste))
+    file_path = join(OUTPUT_FOLDER, filename)
+    return exists(file_path)
+
+
+def store_paste_date_to_disk(paste):
+    data = paste.get()
+    data["Date"] = str(data["Date"])
+    filename = "{}.json".format(get_paste_hash(paste))
+    file_path = join(OUTPUT_FOLDER, filename)
+    with open(file_path, 'w') as f:
+        json.dump(data, f, indent=4)
 
 
 if __name__ == '__main__':
-    for paste in simple_pastebin_parser.get_posts(True, 30):
-        # print("href: ", href)
-        print("Title: ", paste.Title)
-        print("Author: ", paste.Author)
-        print("date: ", paste.Date)
-        print(paste.Content)
-        print("*" * 20)
-        print("*" * 20)
+    create_output_folder_if_doesnt_exist()
+    for paste in simple_pastebin_parser.get_posts(True, 60*2):
+        if not does_paste_file_already_exist(paste):
+            store_paste_date_to_disk(paste)
