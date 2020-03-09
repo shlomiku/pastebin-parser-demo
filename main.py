fbb684a6fefbf8d0c388f0139c11279bbe22d6ec
Shlomi Kushchi <shlomik@gmail.com>
2020-03-09 19:22:21 +0200

    now we store pastes to redis as well to the disk

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import json
+import redis
+
 from os.path import exists, join
-from os import getcwd,mkdir
+from os import getcwd, mkdir
 
 import simple_pastebin_parser
 
 OUTPUT_FOLDER = join(getcwd(), "OUTPUT")
+r = redis.StrictRedis(host='192.168.99.100', port=6377, db=2)
 
 
 def create_output_folder_if_doesnt_exist():
@@ -27,8 +30,15 @@ def store_paste_date_to_disk(paste):
         json.dump(data, f, indent=4)
 
 
+def store_paste_to_redis(paste):
+    data = paste.get()
+    data["Date"] = str(data["Date"])
+    r.hmset(paste.id, data)
+
+
 if __name__ == '__main__':
     create_output_folder_if_doesnt_exist()
     for paste in simple_pastebin_parser.get_pastes(should_stream=True, sampling_frequency=60*2):
         if not does_paste_file_already_exist(paste):
             store_paste_date_to_disk(paste)
+            store_paste_to_redis(paste)
